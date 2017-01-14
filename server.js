9550ca100fe83c15403f0e7fb5a7ba8d4dc7b789
Angel Salazar Marcatoma <Angel Salazar Marcatoma>
2017-01-14 09:56:13 -0600

    fixed http request over https middleware

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var app = express();
 // Redirect traffic that uses http protocol
 // throughout https
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
