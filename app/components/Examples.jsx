efff30d2d5519544a884b5019a11c4beb9b9f63e
Angel Salazar Marcatoma <Angel Salazar Marcatoma>
2017-04-02 14:43:12 -0500

    updateing examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
-
+var {Link} = require("react-router");
 var Examples = React.createClass({
   render :  function () {
     return (
       <div>
-        I am examples
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out;</p>
+        <ol>
+          <li><Link to='/?location=Monterrey'>Monterrey, NL</Link></li>
+          <li><Link to='/?location=Tampico'>Tampico, Tamps</Link></li>
+        </ol>
       </div>
     );
   }
